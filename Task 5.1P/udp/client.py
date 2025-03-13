b65f36f9db2e308b9ecbae9357322379bc6d5b0b
yashpinder26 <yashpindersaini@gmail.com>
2025-03-13 16:04:50 +0530

    Update client.py

--- Task 5.1P/udp/client.py
+++ Task 5.1P/udp/client.py
@@ -1,7 +1,12 @@
-import socket
-ob=socket.socket()
-ob.connect(
-    ("127.0.0.1",11500)
-)
-print(ob.recv(1024).decode())
-ob.send(input("Enter the msg: ").encode())
+import socket  
+
+ob = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  
+server_address = ("127.0.0.1", 11500)  
+
+message = "Hello"  
+ob.sendto(message.encode(), server_address)  
+
+data, _ = ob.recvfrom(1024)  
+print("Received from server:", data.decode())  
+
+ob.close()  
